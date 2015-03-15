f3616d0bef51627ecea972d340594d2ebad46934
Rebecca Skinner <traybaby@gmail.com>
2015-03-15 18:14:38 +0800

    Section 4.4.2: Redirect the users back to the projects page if they   try going to a project that does not exist

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -20,16 +22,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
-
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -40,7 +38,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been deleted."
@@ -48,6 +45,13 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
 
   def project_params
     params.require(:project).permit(:name, :description)
--- ticketee/spec/controllers/projects_controller_spec.rb
+++ ticketee/spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  it "handles a missing project correctly" do
+    get :show, id: "not-here"
 
+    expect(response).to redirect_to(projects_path)
+
+    message = "The project you were looking for could not be found."
+    expect(flash[:alert]).to eq message
+  end
 end
