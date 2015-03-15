b9d6d11c53a28bcacf8a7ca5247b1dd18a48e98b
Rebecca Skinner <traybaby@gmail.com>
2015-03-15 11:18:50 +0800

    Section 3.4.4: Add title functionality for project show page

--- ticketee/spec/features/creating_projects_spec.rb
+++ ticketee/spec/features/creating_projects_spec.rb
@@ -11,5 +11,11 @@ RSpec.feature "Users can create new projects" do
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
+
+    project = Project.find_by(name: "Sublime Text 3")
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Sublime Text 3 - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
