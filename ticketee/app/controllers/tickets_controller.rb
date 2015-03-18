bfc796fd5ccee3573f21bfd2381beccdf7edfd4e
Rebecca Skinner <traybaby@gmail.com>
2015-03-18 21:04:34 +0800

    Section 5.3.3: Tickets can now be edited

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -21,6 +21,19 @@ class TicketsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
+  end
+
   private
 
   def ticket_params
---
+++ ticketee/spec/features/editing_tickets_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "Users can edit existing tickets" do
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket)  { FactoryGirl.create(:ticket, project: project) }
+
+  before do
+    visit project_ticket_path(project, ticket)
+    click_link "Edit Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Make it really shiny!"
+    click_button "Update Ticket"
+
+    expect(page).to have_content "Ticket has been updated."
+
+    within("#ticket h2") do
+      expect(page).to have_content "Make it really shiny!"
+      expect(page).not_to have_content ticket.name
+    end
+  end
+
+  scenario "with invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Ticket"
+
+    expect(page).to have_content "Ticket has not been updated."
+  end
+end
