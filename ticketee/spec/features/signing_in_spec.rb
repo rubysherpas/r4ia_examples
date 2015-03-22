cc56601dfd7ab07d0757c72a51c70a6dc8530bc6
Rebecca Skinner <traybaby@gmail.com>
2015-03-22 14:06:16 +0800

    Section 6.3.2: Styled sign up and sign in forms

--- ticketee/spec/features/signing_in_spec.rb
+++ ticketee/spec/features/signing_in_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Users can sign in" do
     click_link "Sign in"
     fill_in "Email", with: user.email
     fill_in "Password", with: "password"
-    click_button "Log in"
+    click_button "Sign in"
 
     expect(page).to have_content "Signed in successfully."
     expect(page).to have_content "Signed in as #{user.email}"
