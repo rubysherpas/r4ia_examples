409d0f087193149d82cef889beb8b95f31d14dea
Rebecca Skinner <traybaby@gmail.com>
2015-03-26 09:35:34 +0800

    Section 8.1.6: Handle authorization errors gracefully

--- ticketee/app/controllers/application_controller.rb
+++ ticketee/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    redirect_to root_path, alert: "You aren't allowed to do that."
+  end
 end
--- ticketee/spec/controllers/projects_controller_spec.rb
+++ ticketee/spec/controllers/projects_controller_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe ProjectsController, type: :controller do
     message = "The project you were looking for could not be found."
     expect(flash[:alert]).to eq message
   end
+
+  it "handles permission errors by redirecting to a safe place" do
+    allow(controller).to receive(:current_user)
+
+    project = FactoryGirl.create(:project)
+    get :show, id: project
+
+    expect(response).to redirect_to(root_path)
+    message = "You aren't allowed to do that."
+    expect(flash[:alert]).to eq message
+  end
 end
