141a326cc005938e2a2a6f4c7a7dafb161ade5ca
Rebecca Skinner <traybaby@gmail.com>
2015-04-05 18:29:55 +0800

    Section 13.6: Add Mailgun configuration

--- ticketee/config/environments/production.rb
+++ ticketee/config/environments/production.rb
@@ -63,6 +63,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.delivery_method = :smtp
+
+  host = "r4ia-ticketee.herokuapp.com"
+
+  ActionMailer::Base.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         host,
+    authentication: :plain,
+  }
+
+  config.action_mailer.default_url_options = {
+    host: host
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
