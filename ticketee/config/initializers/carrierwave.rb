8295154517e98d78d5419d2013572edfa6a66b57
Rebecca Skinner <traybaby@gmail.com>
2015-04-05 17:09:47 +0800

    Section 13.3.3: Configuring CarrierWave for 12factor

--- ticketee/config/initializers/carrierwave.rb
+++ ticketee/config/initializers/carrierwave.rb
@@ -1,3 +1,16 @@
 CarrierWave.configure do |config|
   config.root = Rails.root
+
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["S3_KEY"],
+      aws_secret_access_key: ENV["S3_SECRET"],
+      region: ENV["S3_REGION"]
+    }
+    config.fog_directory = ENV["S3_BUCKET"]
+  else
+    config.storage = :file
+  end
 end
